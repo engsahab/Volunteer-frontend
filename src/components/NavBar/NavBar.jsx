b7e8882f25abc4eec583131694df137dc110b983
engsahab <sahabalfahad6@gmail.com>
2025-10-29 11:24:44 +0300

    Create NavBar component

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router';
+import './NavBar.css'; 
+
+function NavBar() {
+  return (
+    <nav className="navbar-container">
+      <Link className="nav-link" to="/opportunities">
+        All Opportunities
+      </Link>
+      <Link className="nav-link" to="/opportunities/new">
+        Add Opportunity
+      </Link>
+    </nav>
+  );
+}
+
+export default NavBar;
