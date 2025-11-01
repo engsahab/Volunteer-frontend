f64358f0777bfb0e907c8f8544bee6b7d143c5b5
engsahab <sahabalfahad6@gmail.com>
2025-11-01 23:23:48 +0300

    Add NavBar component and client-side search functionality

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,17 +1,54 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './NavBar.css';
+import LogOutButton from '../LogOutButton/LogOutButton';
 
-function NavBar() {
+
+function NavBar({ user, setUser }) { 
   return (
-
     <nav className="navbar-container">
+     
       <Link className="nav-link" to="/opportunities">
         All Opportunities
       </Link>
-      <Link className="nav-link" to="/opportunities/new">
-        Add Opportunity
-      </Link>
+      
+      {user ? (
+        <>
+
+          {user.is_staff && (
+            <>
+              <Link className="nav-link" to="/opportunities/new">
+                Add Opportunity
+              </Link>
+              <Link className="nav-link" to="/admin/dashboard">
+                Admin Dashboard
+              </Link>
+            </>
+          )}
+          
+           
+
+          {user && !user.is_staff && (
+            <Link className="nav-link" to="/profile">
+              My Profile
+           </Link>
+          )}
+
+          <LogOutButton setUser={setUser} /> 
+        </>
+      ) : (
+        <>
+          
+          <div className="auth-links">
+            <Link className="nav-link" to="/login">
+              Login
+            </Link>
+            <Link className="nav-link" to="/signup">
+              Sign Up
+            </Link>
+          </div>
+        </>
+      )}
     </nav>
   );
 }
